978ca8e596478662bfc8c291d16f19aa9bec4650
SriLakshmi-3 <2100032462@kluniversity.in>
2024-06-21 22:34:19 +0530

    this is my mern commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './Navbar';
+import Login from './login';
 import './App.css';
+import About from './about';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div id="home" style={{ padding: '50px', textAlign: 'center' }}>
+        <h1>Welcome to MyApp</h1>
+      </div>
+      <div id="about" style={{ padding: '50px', textAlign: 'center' }}>
+        <About/>
+      </div>
+      <div id="contact" style={{ padding: '50px', textAlign: 'center' }}>
+        <h2>Contact Us</h2>
+        <p>This is the Contact section.</p>
+      </div>
+      <div id="login" style={{ padding: '50px', textAlign: 'center' }}>
+        <Login />
+      </div>
     </div>
   );
 }
